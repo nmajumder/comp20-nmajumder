bfbe2bf472ca1f6eac80392bcf60cc4576e0f0ff
nmajumder <nathan.majumder@tufts.edu>
2015-03-02 18:21:03 -0500

    displaying ms pacman in maze

--- mspacman/game.js
+++ mspacman/game.js
@@ -5,5 +5,6 @@ function init() {
 	img.src = 'pacman10-hp-sprite.png';
 	img.onload = function() {
 		ctx.drawImage(img, 322, 2, 464, 135, 0, 0, 464, 135);
+		ctx.drawImage(img, 81, 22, 16, 15, 35, 30, 16, 14);
 	}
 }
