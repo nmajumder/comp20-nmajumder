91ed28510247dc7d7625e771d803e2e0bfa47186
nmajumder <nathan.majumder@tufts.edu>
2015-03-05 13:18:41 -0500

    modified lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			msgDiv = document.getElementById("messages");
+			parsed = JSON.parse(request.responseText);
+			for (i = 0; i < parsed.length; i++) {
+				msgDiv.innerHTML += "<p>" + parsed[i]['content'] + "</p>";
+			}		
+		} else if (request.readyState == 4 && request.status != 200) {
+			alert("Error");
+		}
+	}
+	request.send();
+}
