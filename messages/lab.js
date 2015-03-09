75e9fe953984c0fcebfb2feda7eb44fdb91cd60f
nmajumder <nathan.majumder@tufts.edu>
2015-03-09 16:45:31 -0400

    parse json

--- messages/lab.js
+++ messages/lab.js
@@ -2,14 +2,14 @@ function parse() {
 	request = new XMLHttpRequest();
 	request.open("GET", "data.json", true);
 	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status == 200) {
+		if (request.readyState == 4 /*&& request.status == 200*/) {
 			msgDiv = document.getElementById("messages");
 			parsed = JSON.parse(request.responseText);
 			for (i = 0; i < parsed.length; i++) {
-				msgDiv.innerHTML += "<p>" + parsed[i]['content'] + "</p>";
+				msgDiv.innerHTML += "<p>" + parsed[i]['content'] + "-" + parsed[i]['username'] + "</p>";
 			}		
-		} else if (request.readyState == 4 && request.status != 200) {
-			alert("Error");
+		/*} else if (request.readyState == 4 && request.status != 200) {
+			alert("Error");*/
 		}
 	}
 	request.send();
